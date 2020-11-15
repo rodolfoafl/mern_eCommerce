d8948ebeefa8aa15a732d03d6d8a58ac686c49c6
Rodolfo <rodolfoafl@gmail.com>
2020-11-14 22:16:25 -0300

    Installed React Bootstrap & created Header/Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to YouR Shop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to YouR Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
