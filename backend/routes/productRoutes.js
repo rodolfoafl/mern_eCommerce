925aba62cfdc9eba8580004abaae69007c43d3b3
Rodolfo <rodolfoafl@gmail.com>
2020-12-01 16:56:52 -0300

    Created ProductRemove endpoint & route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,15 @@ import express from "express";
 import {
   getProductById,
   getProducts,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, checkAdmin, deleteProduct);
 
 export default router;
