d7d27db7112880b0cdc96b78bccdb35882848327
Rodolfo <rodolfoafl@gmail.com>
2020-12-07 16:55:55 -0300

    Implemented CreateProductReview endpoint & route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ import {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, checkAdmin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
