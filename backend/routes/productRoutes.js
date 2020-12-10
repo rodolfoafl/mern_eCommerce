e4662e60dee61cbf5a755e80a602912b79881273
Rodolfo <rodolfoafl@gmail.com>
2020-12-10 01:25:34 -0300

    Created GetTopProducts endpoint & route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,10 +6,12 @@ import {
   updateProduct,
   createProduct,
   createProductReview,
+  getTopProducts,
 } from "../controllers/productController.js";
 import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
+router.get("/top", getTopProducts);
 router.route("/").get(getProducts).post(protect, checkAdmin, createProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
 router
